b4d750863a90c78aa86973b0cbfc7a543df5d946
johnny <johann.gollan@rub.de>
2025-05-10 14:02:25 +0200

    small fixes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,25 +10,21 @@ import Register from "./components/Register.jsx";
 import Dashboard from "./components/Dashboard.jsx";
 import UserPanel from "./components/UserPanel.jsx";
 import AdminPanel from "./components/AdminPanel.jsx";
-import { AuthProvider } from './components/AuthContext';
+import { AuthProvider } from './components/AuthContext.jsx'; // Updated path
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/user-panel" element={<UserPanel/>} />
-                <Route path="/admin-panel" element={<AdminPanel />} />
-
-                <Route path="/dashboard" element={
-                    <AuthProvider>
-                        <Dashboard />
-                    </AuthProvider>
-                } />
-
-            </Routes>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/" element={<App />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/user-panel" element={<UserPanel/>} />
+                    <Route path="/admin-panel" element={<AdminPanel />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                </Routes>
+            </AuthProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
---
+++ src/main/java/com/example/ssbu_league/dto/UserDTOMapper.java
@@ -0,0 +1,28 @@
+package com.example.ssbu_league.dto;
+
+import com.example.ssbu_league.models.AppUser;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDTOMapper {
+
+    public UserScoreDTO toUserScoreDTO(AppUser appUser) {
+        return new UserScoreDTO(
+                appUser.getGamerTag(),
+                appUser.getScore()
+                // TODO Update accordingly to UserToDTOMapper
+        );
+    }
+
+    /* I decided to ditch the UserDTO for the moment being to simplify user data changes by just using the actual
+     * AppUser objects in transfer
+
+    public AppUserDTO toAppUserDTO(AppUser appUser) {
+        return new AppUserDTO(
+                appUser.getUsername(),
+                appUser.getPassword(),
+                appUser.getGamerTag(),
+                appUser.getRole()
+        );
+    } */
+}
